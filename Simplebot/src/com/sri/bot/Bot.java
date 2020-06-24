726a38dad3417e04307766295e7edf5be3361c21
Srinath <gopathisrinath@gmail.com>
2020-06-24 08:40:20 +0530

    count is updated

--- Simplebot/src/com/sri/bot/Bot.java
+++ Simplebot/src/com/sri/bot/Bot.java
@@ -25,6 +25,13 @@ public class Bot {
 		int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 
 		System.out.println("Your age is " + yourAge + ";" + "that's a good time to start programming!");
+		System.out.println("Now I will prove to you that I can count to any number you want.");
+
+		int count = sc.nextInt();
+
+		for (int i = 0; i <= count; i++) {
+			System.out.println(i + "!");
+		}
 	}
 
 }
